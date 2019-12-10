f44ce30a785f3eae74c7e8ce633fc36864911c85
sorXCode <sorxcode@gmail.com>
2019-12-11 00:01:45 +0100

    added provider

--- tradin/lib/main.dart
+++ tradin/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:tradin/http/user.dart';
+import 'package:tradin/models/login.dart';
+import 'package:tradin/models/signup.dart';
+import 'package:tradin/router.dart';
 import 'package:tradin/ui/home/home.dart';
+import 'package:tradin/ui/user/login.dart';
 import 'package:tradin/ui/user/widgetsConstants.dart';
 
 void main() async {
@@ -14,11 +20,24 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget with WidgetsConstants {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LoginState()),
+        ChangeNotifierProvider(create: (_) => SignUpState()),
+        ChangeNotifierProvider(create: (_) => AuthService()),
+      ],
+      child: MaterialApp(
+          onGenerateRoute: Router.generateRoute,
+          debugShowCheckedModeBanner: false,
+          home: Builder(
+            builder: (context) =>
+                Provider.of<AuthService>(context).userLoggedIn()
+                    ? Home()
+                    : Login(),
+          )),
     );
   }
 }
